48c6beddb5ae9ac58cb0e2b81bd8be529c8fe26a
edwardzhong <edwardloveyou@gmail.com>
2019-03-20 00:28:11 +0800

    reset db

--- server/config/chatdb.sql
+++ server/config/chatdb.sql
@@ -5,62 +5,93 @@ drop table if exists `user`;
 CREATE TABLE `user` (
   `id` char(36) NOT NULL DEFAULT '' COMMENT '主键',
   `name` varchar(50) NOT NULL COMMENT '用户名',
+  `num` int(8) NOT NULL COMMENT '用户号码',
   `salt` varchar(13) DEFAULT NULL COMMENT '加密的盐',
   `hash_password` varchar(64) DEFAULT NULL COMMENT '加密后的密码',
   `email` varchar(50) DEFAULT NULL COMMENT 'email地址',
   `nick` varchar(50) DEFAULT NULL COMMENT '昵称',
   `avatar` varchar(50) DEFAULT NULL COMMENT '头像',
   `signature` varchar(100) DEFAULT NULL COMMENT '个性签名',
+  `is_onlne` tinyint(1) DEFAULT '0' COMMENT '是否在线',
+  `is_visible` tinyint(1) DEFAULT '0' COMMENT '是否隐身',
   `is_admin` tinyint(1) DEFAULT '0' COMMENT '是否管理员',
   `is_block` tinyint(1) DEFAULT '0' COMMENT '是否禁用',
   `create_date` int(10) unsigned DEFAULT NULL COMMENT '注册时间',
   `update_date` int(10) unsigned DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment '用户表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表'; 
 
-drop table if exists `article`;
-create table `article`(
-`id` int not null auto_increment primary key comment '主键',
-`title` varchar(100) comment '标题',
-`content` text comment '内容',
-`author_id` char(36) not null comment '作者id',
-`is_good` tinyint(1) default 0 comment '是否精品',
-`is_top` tinyint(1) default 0 comment '是否置顶',
-`create_date` int unsigned comment '创建时间',
-`update_date` int unsigned comment '更新时间'
-) comment '文章表';
-
-drop table if exists `tag`;
-create table `tag`(
-`id` int not null auto_increment primary key comment '主键',
-`name` varchar(20) not null comment '标签名称'
-) comment '标签表';
+drop table if exists `group`;
+CREATE TABLE `group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `group_avatar` varchar(50) DEFAULT NULL COMMENT '群图像',
+  `create_id` char(36) NOT NULL COMMENT '群主id',
+  `name` varchar(20) NOT NULL COMMENT '组名',
+  `create_date` int(10) unsigned DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='群组表';
 
 drop table if exists `message`;
-create table `message`(
-`id` int not null auto_increment primary key comment '主键',
-`content` varchar(100) not null comment '内容',
-`type` tinyint(1) default 0 comment '类型',
-`user_id` char(36) NOT NULL COMMENT '用户id',
-`topic_id` int comment '主题id',
-`reply_id` int comment '回复id',
-`is_read` tinyint(1) default 0 comment '是否已经读过',
-`create_date` int unsigned comment '创建时间'
-) comment '消息表';
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `content` varchar(100) NOT NULL COMMENT '内容',
+  `type` tinyint(1) DEFAULT 0 COMMENT '类型:(0 用户 1 组群)',
+  `send_id` char(36) NOT NULL COMMENT '发送用户id',
+  `create_date` int(10) unsigned DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='消息表';
 
-drop table if exists `tag_article`;
-CREATE TABLE `tag_article` (
+drop table if exists `friend_apply`;
+CREATE TABLE `friend_apply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `from_id` char(36) NOT NULL COMMENT '申请方id',
+  `to_id` char(36) NOT NULL COMMENT '接受方id',
+  `status` tinyint(1) DEFAULT 0 COMMENT '状态(0 待处理  1 已同意 2 已拒绝)',
+  `apply_message` char(20) NOT NULL COMMENT '附加消息',
+  `reply` char(20) NOT NULL COMMENT '回复消息',
+  `update_date` int(10) unsigned DEFAULT NULL COMMENT '更新时间',
+  `create_date` int(10) unsigned DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='好友申请表';
+
+drop table if exists `user_friend`;
+CREATE TABLE `user_friend` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `user_id` char(36) NOT NULL COMMENT '用户id',
-  `article_id` int NOT NULL COMMENT '文章id',
-  `tag_id` int NOT NULL COMMENT '标签id',
-  PRIMARY KEY (`user_id`,`article_id`,`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='标签文章连接表'; 
+  `friend_id` char(36) NOT NULL COMMENT '好友id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户好友连接表';
+
+drop table if exists `user_group`;
+CREATE TABLE `user_group` (
+  `user_id` char(36) NOT NULL COMMENT '用户id',
+  `group_id` int(11) NOT NULL COMMENT '组id',
+  PRIMARY KEY (`user_id`,`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户组群连接表';
+
+drop table if exists `user_message`;
+CREATE TABLE `user_message` (
+  `user_id` char(36) NOT NULL COMMENT '接收用户id',
+  `message_id` int(11) NOT NULL COMMENT '消息id',
+  `is_read` tinyint(1) DEFAULT 0 COMMENT '是否已经读过:(0 没有 1 读过)',
+  PRIMARY KEY (`user_id`,`message_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户消息连接表';
+
+drop table if exists `group_message`;
+CREATE TABLE `group_message` (
+  `group_id` char(11) NOT NULL COMMENT '接收组id',
+  `message_id` int(11) NOT NULL COMMENT '消息id',
+  `is_read` tinyint(1) DEFAULT 0 COMMENT '是否已经读过:(0 没有 1 读过)',
+  PRIMARY KEY (`group_id`,`message_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='组群消息连接表';
 
 /* 触发器 和 存储过程 */
 drop trigger if exists `user_insert`; 
 drop trigger if exists `user_update`; 
-drop trigger if exists `article_insert`; 
-drop trigger if exists `article_update`; 
+drop trigger if exists `group_insert`; 
+drop trigger if exists `message_insert`; 
+drop trigger if exists `friend_apply_insert`; 
+drop trigger if exists `friend_apply_update`; 
 drop procedure if exists `getpage`;
 
 -- Do nothing, if appear 'ERROR 1064 (42000): ...', it's ok !
@@ -73,14 +104,14 @@ delimiter $$
 create trigger `user_insert` before insert on `user`
 for each ROW
 begin
-if (new.id='' or new.id is null)
-	then set new.id=uuid();
+if (new.id = '' or new.id is null)
+	then set new.id = uuid();
 end if;
-if (new.`create_date`=0 or new.`create_date` is null)
-	then set new.`create_date`=unix_timestamp(now());
+if (new.`create_date` = 0 or new.`create_date` is null)
+	then set new.`create_date` = unix_timestamp(now());
 end if;
-if (new.`update_date`=0 or new.`update_date` is null)
-	then set new.`update_date`=unix_timestamp(now());
+if (new.`update_date` = 0 or new.`update_date` is null)
+	then set new.`update_date` = unix_timestamp(now());
 end if;
 END
 $$
@@ -90,38 +121,60 @@ delimiter $$
 create trigger `user_update` before update on `user`
 for each ROW
 begin
-if ((new.`name`<>old.`name`) or (new.`name` is not null and old.`name` is null) 
-	or (new.`email`<>old.`email`) or (new.`email` is not null and old.`email` is null)
-	or (new.`nick`<>old.`nick`) or (new.`nick` is not null and old.`nick` is null)
-	or (new.`avatar`<>old.`avatar`) or (new.`avatar` is not null and old.`avatar` is null)
-	or (new.`signature`<>old.`signature`) or (new.`signature` is not null and old.`signature` is null))
-	then set new.`update_date`=unix_timestamp(now());
+if ((new.`name` <> old.`name`) or (new.`name` is not null and old.`name` is null) 
+	or (new.`email` <> old.`email`) or (new.`email` is not null and old.`email` is null)
+	or (new.`nick` <> old.`nick`) or (new.`nick` is not null and old.`nick` is null)
+	or (new.`avatar` <> old.`avatar`) or (new.`avatar` is not null and old.`avatar` is null)
+	or (new.`signature` <> old.`signature`) or (new.`signature` is not null and old.`signature` is null))
+	then set new.`update_date` = unix_timestamp(now());
 end if;
 END
 $$
 
--- article表insert触发器
+-- group表insert触发器
 delimiter $$
-create trigger `article_insert` before insert on `article`
+create trigger `group_insert` before insert on `group`
 for each ROW
 begin
-if (new.`create_date`=0 or new.`create_date` is null)
-	then set new.`create_date`=unix_timestamp(now());
-end if;
-if (new.`update_date`=0 or new.`update_date` is null)
-	then set new.`update_date`=unix_timestamp(now());
+if (new.`create_date` = 0 or new.`create_date` is null)
+	then set new.`create_date` = unix_timestamp(now());
 end if;
 END
 $$
 
--- article表update触发器
+-- message表insert触发器
 delimiter $$
-create trigger `article_update` before update on `article`
+create trigger `message_insert` before insert on `message`
 for each ROW
 begin
-if ((new.`title`<>old.`title`) or (new.`title` is not null and old.`title` is null) 
-	or (new.`content`<>old.`content`) or (new.`content` is not null and old.`content` is null))
-then set new.`update_date`=unix_timestamp(now());
+if (new.`create_date` = 0 or new.`create_date` is null)
+	then set new.`create_date` = unix_timestamp(now());
+end if;
+END
+$$
+
+-- friend_apply表insert触发器
+delimiter $$
+create trigger `friend_apply_insert` before insert on `friend_apply`
+for each ROW
+begin
+if (new.`create_date` = 0 or new.`create_date` is null)
+	then set new.`create_date` = unix_timestamp(now());
+end if;
+if (new.`update_date` = 0 or new.`update_date` is null)
+	then set new.`update_date` = unix_timestamp(now());
+end if;
+END
+$$
+
+-- friend_apply表update触发器
+delimiter $$
+create trigger `friend_apply_update` before update on `friend_apply`
+for each ROW
+begin
+if ((new.`status` <> old.`status`) or (new.`status` is not null and old.`status` is null) 
+	or (new.`reply` <> old.`reply`) or (new.`reply` is not null and old.`reply` is null))
+	then set new.`update_date` = unix_timestamp(now());
 end if;
 END
 $$
