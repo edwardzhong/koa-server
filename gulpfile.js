7886e3c85bf10376752ae89910d87ec86ed56b8b
edwardzhong <zhongjfgd@163.com>
2020-02-04 01:26:12 +0800

    tab

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,26 @@ const nodemon = require('gulp-nodemon');
 const tsProject = ts.createProject('tsconfig.json');
 
 function clean(cb) {
-	return del(['dist'], cb);
+  return del(['dist'], cb);
 }
 
 function toJs() {
-	return src('src/**/*.ts')
-		.pipe(tsProject())
-		.pipe(dest('dist'));
+  return src('src/**/*.ts')
+    .pipe(tsProject())
+    .pipe(dest('dist'));
 }
 
 function runNodemon() {
-	nodemon({
-		inspect: true,
-		script: 'src/app.ts',
-		watch: ['src'],
-		ext: 'ts',
-		env: { NODE_ENV: 'development' },
-		// tasks: ['build'],
-	}).on('crash', () => {
-		console.error('Application has crashed!\n');
-	});
+  nodemon({
+    inspect: true,
+    script: 'src/app.ts',
+    watch: ['src'],
+    ext: 'ts',
+    env: { NODE_ENV: 'development' },
+    // tasks: ['build'],
+  }).on('crash', () => {
+    console.error('Application has crashed!\n');
+  });
 }
 
 // series   从左至右依次串行执行任务
